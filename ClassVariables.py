97a617959550fbc302c9c7a1baa86bef55f87142
JosephThachilGeorge <meghnajoseph118@gmail.com>
2021-01-25 17:57:43 +0100

    New Examples

--- ClassVariables.py
+++ ClassVariables.py
@@ -1,5 +1,7 @@
 class Employee:
 
+    raise_amount=1.04
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
@@ -8,6 +10,8 @@ class Employee:
 
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
+    def apply_raise(self):
+        self.pay=int(self.pay* self.raise_amount)
 
 emp_1 = Employee('Joseph', 'thachil', 50000)
 emp_2 = Employee('Paul', 'Varghese', 60000)
@@ -19,3 +23,10 @@ print(emp_1.pay)
 print(emp_2.pay)
 print(emp_1.fullname())
 print(emp_2.fullname())
+
+print(Employee.raise_amount)
+emp_1.apply_raise()
+print(emp_1.pay)
+emp_2.raise_amount=1.09
+print(emp_2.raise_amount)
+print(emp_2.pay)
